3ca66aff7198358433041b1cee20d6c4b9a800f6
AjdinMasic-code <ajdinmasic.coding@gmail.com>
2016-12-01 23:18:06 -0700

    Updates to back to top button
    Rearranged positioning and indents of code, made values more dynamic,
    added timer features so the CPU doesn't get bogged down, fixed issue
    with button not dissappearing after refreshing page towards the bottom
    of page, and increased overall performance of website.

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,75 +1,76 @@
-$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 800, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
-
-
-$("a.navbar-brand[href^='#']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 800, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
-
-
 $(document).ready(function() {
 
+  $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
+
+
+  $("a.navbar-brand[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
+
+/* The start of back to top button */
+      var timeout = null,
+          divider =2,
+          minimumScrollbarPosition = 900;
+
+    $(window).scroll(function () {
+        if (!timeout) {
+            timeout = setTimeout(function () {            
+                clearTimeout(timeout);
+                timeout = null;
+                checkPosition();
+
+            }, 20);
+        }
+    });
+
     function checkPosition() {
-        if ($(this).scrollTop() > 200) { // Checks if the value of scrollTop is greater than 200
+        if ($(this).scrollTop() >= minimumScrollbarPosition) { // Checks if the value of scrollTop is greater than 200
             $('#back-to-top').fadeIn(500); // Fades in if true
         } else {
             $('#back-to-top').fadeOut(300);// Otherwise fades out
         }
     }
 
-var target = $("#back-to-top").offset().top,
-    timeout = null;
+        // Animate the scroll to top
+        $('#back-to-top').click(function(event) {
+            event.preventDefault();
+            var scrollSpeed = $(window).scrollTop() / divider;
 
-$(window).scroll(function () {
-    if (!timeout) {
-        timeout = setTimeout(function () {            
-            clearTimeout(timeout);
-            timeout = null;
-            if ($(window).scrollTop() >= target) {
-                checkPosition();
-            }
-        }, 300);
-    }
-});
-
-    // Animate the scroll to top
-    $('#back-to-top').click(function(event) {
-        event.preventDefault();
-
-        $('html, body').animate({scrollTop: 0}, 2000); // sets scrollTop value to 0(top of page) 2000 references speed
-    })
+            $('html, body').animate({scrollTop: 0}, scrollSpeed); // sets scrollTop value to 0(top of page) scrollSpeed references speed
+        })
 
 });
