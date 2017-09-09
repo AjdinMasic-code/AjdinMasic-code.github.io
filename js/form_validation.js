669c29c2de4a139173f7f30b7eed42802bb66b0e
AjdinMasic-code <ajdinmasic.coding@gmail.com>
2017-09-08 19:51:26 -0600

    Changes

--- js/form_validation.js
+++ js/form_validation.js
@@ -2,7 +2,15 @@ var $ = function(id) {
     return document.getElementById(id);
 }
 
-var contactUs = function() {
+var pattern = /^\d{3}.\d{3}.\d{4}/;//regex called globally
+
+function showInput() {
+    $('phone-number-display').innerHTML = 
+                $("phone_number").value;
+}
+
+
+function contactUs() {
 	var emailAddress1 = $("email_address1").value;
 	var emailAddress2 = $("email_address2").value;
 	var isValid = true;
@@ -21,7 +29,21 @@ var contactUs = function() {
 		$("first_name_error").firstChild.nodeValue = 
                         "This field is required.";
 		isValid = false;
-	} else { $("first_name_error").firstChild.nodeValue = ""; } 
+	} else { $("first_name_error").firstChild.nodeValue = ""; }
+    
+    if($("contact_form").contains($("phone_number"))){
+        if ($("phone_number").value === "") {
+                $("phone_number_error").firstChild.nodeValue = 
+                                "This field is required.";
+                isValid = false;
+        } else if (!(pattern.test($("phone_number").value))){
+             $("phone_number_error").firstChild.nodeValue = "Error must contain . or -";
+            isValid=false;
+        }
+        else { 
+            $("phone_number_error").firstChild.nodeValue = "";  
+        }         
+    }
     
     if($("contact_form").contains($("textarea"))){
         if($("textarea").value === "") {
@@ -29,10 +51,25 @@ var contactUs = function() {
         isValid = false;
         } else {$("textarea_error").firstChild.nodeValue = "";}
     }
+    
+
 	
 	if (isValid) {
-		$("contact_form").submit(); 
+		$("contact_form").submit();
+        $("phone-number-display").innerHTML = ($("phone_number").value);
+            // Store your data.
+        
+        
+        sessionStorage.setItem("data1", $("phone_number").value);
+        
+        sessionStorage.setItem("data2", $("first_name").value);
+        
+        sessionStorage.setItem("data3", emailAddress1);
+    
+    
 	}
+    
+    
 }
 
 window.onload = function() {
